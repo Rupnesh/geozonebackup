7e2b742f339bb37254b6a352adb220933a81ab47
Alexandru Lazar <alextkd123@yahoo.com>
2017-09-27 18:42:55 +0300

    [client]: fix protocol

--- client/src/index.js
+++ client/src/index.js
@@ -7,9 +7,9 @@ import registerServiceWorker from './registerServiceWorker';
 
 import { SocketProvider } from 'socket.io-react';
 import io from 'socket.io-client';
-let URL = `${window.location.protocol}://${window.location.hostname}:8080`;
+let URL = `${window.location.protocol}//${window.location.hostname}:8080`;
 if (process.env.NODE_ENV === 'production') {
-  URL = `${window.location.protocol}://${window.location.hostname}:${window.location.port}`;
+  URL = `${window.location.protocol}//${window.location.hostname}:${window.location.port}`;
 }
 const socket = io.connect(URL);
 /* global document */
